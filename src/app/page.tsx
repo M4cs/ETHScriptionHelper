eb629b72bee7fe9ca6821d84d3df622c47fee5d4
M4cs <me@maxbridgland.com>
2023-06-21 20:54:35 -0400

    Initial Add

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,228 @@
-import Image from 'next/image'
+"use client";
+import Image from 'next/image';
 import styles from './page.module.css'
+import { ethers } from 'ethers';
+import { ChangeEvent, useMemo, useState } from 'react';
+import { EthereumClient, w3mConnectors, w3mProvider } from '@web3modal/ethereum';
+import { Web3Modal } from '@web3modal/react';
+import { configureChains, createConfig, usePrepareSendTransaction, useSendTransaction, WagmiConfig } from 'wagmi';
+import { arbitrum, mainnet, polygon } from 'wagmi/chains';
+import { Web3Button } from '@web3modal/react';
+import { useAccount } from 'wagmi';
+import { useDebounce } from 'use-debounce';
+import { useEthersProvider } from './ethers';
+import { sendTransaction } from '@wagmi/core';
+import { ToastContainer, toast } from 'react-toastify';
+
+const chains = [arbitrum, mainnet, polygon];
+const projectId = 'e14d37f15600f99222e2ff44adf1bc52';
+
+const { publicClient } = configureChains(chains, [w3mProvider({ projectId })]);
+const wagmiConfig = createConfig({
+  autoConnect: true,
+  connectors: w3mConnectors({ projectId, version: 1, chains }),
+  publicClient
+});
+const ethereumClient = new EthereumClient(wagmiConfig, chains);
 
 export default function Home() {
+  
+  const { connector, isConnecting, isDisconnected } = useAccount();
+
+  const [hex, setHex] = useState<string>('');
+  const [debouncedHex] = useDebounce(hex, 500);
+  const [hexTo, setHexTo] = useState<string>('');
+  const [debouncedHexTo] = useDebounce(hexTo, 500);
+
+  const [text, setText] = useState<string>('');
+  const [debouncedText] = useDebounce(text, 500);
+  const [textType, setTextType] = useState<string>('');
+  const [debouncedTextType] = useDebounce(textType, 500);
+  const [textTo, setTextTo] = useState<string>('');
+  const [debouncedTextTo] = useDebounce(textTo, 500);
+
+  const [image, setImage] = useState<string | ArrayBuffer | null>(null);
+  const [imageTo, setImageTo] = useState<string>('');
+
+  const [json, setJSON] = useState<string>('');
+  const [jsonTo, setJSONTo] = useState<string>('');
+
+  const provider = useEthersProvider();
+
+
+  // const [nftAddress, setNFTAddress] = useState<string>('');
+  // const [tokenID, setTokenID] = useState<string>('');
+  // const [nftTo, setNftTo] = useState<string>('');
+
+  const onImageChange = (event: any) => {
+    if (event?.target?.files && event?.target?.files[0]) {
+      const i = event.target.files[0];
+      var reader = new FileReader();
+      reader.onloadend = function() {
+        setImage(reader.result);
+      }
+      reader.readAsDataURL(i);
+    }
+  }
+
+  const onEthscribeText = async () => {
+    console.log(debouncedTextTo);
+    if (debouncedText == '' || debouncedTextTo == '') {
+      toast.error('Text or To is blank!');
+      return;
+    }
+    if (ethers.isAddress(debouncedTextTo)) {
+      console.log(`0x${ethers.hexlify(ethers.toUtf8Bytes(`data:${debouncedTextType},${debouncedText}`)).slice(2)}`)
+      toast.promise(
+        sendTransaction({
+          value: BigInt(0),
+          to: debouncedTextTo,
+          data: `0x${ethers.hexlify(ethers.toUtf8Bytes(`data:${debouncedTextType},${debouncedText}`)).slice(2)}`
+        }),
+        {
+          success: 'ETHScription Success!',
+          error: `Error ETHScribing...`,
+          pending: `Sending ETHScription...`
+        }
+      ).then(({hash}) => {
+        toast.info(`Txn. Hash: ${hash}`);
+      });
+    } else {
+      toast.error('Invalid Address!');
+    }
+  }
+
+  const onEthscribeRaw = async () => {
+    console.log(debouncedTextTo);
+    if (debouncedHex == '' || debouncedHexTo == '' || !debouncedHexTo.startsWith('0x')) {
+      toast.error('Hex or To is blank or incorrect!');
+      return;
+    }
+    if (ethers.isAddress(debouncedHexTo)) {
+      toast.promise(
+        sendTransaction({
+          value: BigInt(0),
+          to: debouncedHexTo,
+          data: `0x${debouncedHex.slice(2)}`
+        }),
+        {
+          success: 'ETHScription Success!',
+          error: `Error ETHScribing...`,
+          pending: `Sending ETHScription...`
+        }
+      ).then(({hash}) => {
+        toast.info(`Txn. Hash: ${hash}`);
+      })
+    } else {
+      toast.error('Invalid Address!');
+    }
+  }
+
+
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+    <>
+      <WagmiConfig config={wagmiConfig}>
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
 
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
+        <main className={styles.main}>
+          <div className={styles.description}>
+            <p>
+              Get Started With ETHScriptions. Below you can Ethscribe raw hex data and text.<br/>
+              See more information about ETHScriptions and ETHScript images <a className={styles.link} href="https://ethscriptions.com" target='_blank'>here</a>.<br/>
+              This will not charge you anything! It simply formats and sends text data to Ethereum easily for you!<br/>
+              Made by <a className={styles.link} href="https://twitter.com/maxbridgland" target='_blank'>@maxbridgland</a>
+            </p>
+            <br/><br/>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
+            {!connector &&
+              <p>
+                <Web3Button />
+              </p>
+            }
+            {connector &&
+            <>
+              <p>
+                <strong>ETHScribe Text Data</strong>
+                <br/><br/>
+                Text:
+                <input className={styles.input} onChange={(e: ChangeEvent<HTMLInputElement>) => {
+                  setText(e.target.value);
+                }}>
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
+                </input>
+                <br/><br/>
+                Mimetype (e.g: application/json, image/png. Leave blank for text/plain):
+                <input className={styles.input} onChange={(e: ChangeEvent<HTMLInputElement>) => {
+                  setTextType(e.target.value);
+                }}>
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+                </input>
+                <br/><br/>
+                Ethscribe To:
+                <input className={styles.input} onChange={(e: ChangeEvent<HTMLInputElement>) => {
+                  setTextTo(e.target.value);
+                }}>
+
+                </input>
+                <br/><br/>
+                <button onClick={(e) => {
+                  e.preventDefault();
+                  onEthscribeText();
+                }}>Ethscribe</button>
+              </p>
+
+              <p>
+                <strong>ETHScribe Raw Data</strong>
+                <br/><br/>
+                Hex Data:
+                <input className={styles.input} onChange={(e: ChangeEvent<HTMLInputElement>) => {
+                  setHex(e.target.value);
+                }}>
+
+                </input>
+                <br/><br/>
+                Ethscribe To:
+                <input className={styles.input} onChange={(e: ChangeEvent<HTMLInputElement>) => {
+                  setHexTo(e.target.value);
+                }}>
+
+                </input>
+                <br/><br/>
+                <button onClick={(e) => {
+                  e.preventDefault();
+                  onEthscribeText();
+                }}>Ethscribe</button>
+              </p>
+            </>
+            }
+
+            {/* <p>
+              <strong>ETHScript NFT onto Blockchain</strong>
+              <br/><br/>
+              Warning this <strong>WILL BURN YOUR NFT IF IT'S BURNABLE</strong>
+              <br/><br/>
+              NFT Address:
+              <input className={styles.input}>
+
+              </input>
+              <br/><br/>
+              Token ID:
+              <input className={styles.input}>
+
+              </input>
+              <br/><br/>
+              Ethscribe To:
+              <input className={styles.input}>
+
+              </input>
+            </p> */}
+          </div>      
+        </main>
+      </WagmiConfig>
+      <ToastContainer />
+
+      <Web3Modal projectId={projectId} ethereumClient={ethereumClient} />
+    </>
+
   )
 }
